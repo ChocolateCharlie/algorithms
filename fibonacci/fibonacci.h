253a9154249696290a9648ef10596b0d5662741e
ChocolateCharlie <aurore.amrit@gmail.com>
2019-12-12 17:31:35 +0100

    Create fibonacci folder

---
+++ fibonacci/fibonacci.h
@@ -0,0 +1,12 @@
+/*
+ * Longest Increasing Sequence (LIS)
+ * Updated: 2019-12-12
+ * By: ChocolateCharlie
+ */
+
+#ifndef FIBONACCI_H_INCLUDED
+#define FIBONACCI_H_INCLUDED
+
+
+
+#endif // FIBONACCI_H_INCLUDED
